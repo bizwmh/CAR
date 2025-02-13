c5a4116fbcb17f3d857cdbef6149a763039fc6cd
it-lo <it-lo@WMH-SPECTRE.fritz.box>
2025-02-13 05:36:05 +0100

    Class Dummy added

--- src/main/java/biz/car/util/Delay.java
+++ src/main/java/biz/car/util/Delay.java
@@ -6,12 +6,61 @@
 
 package biz.car.util;
 
+import java.util.function.Function;
+
+import biz.car.SYS;
+
 /**
  * Creates a wait in current thread.
  * 
  * @version 1.0.0 16.10.2024 15:24:10
  */
 public interface Delay {
+
+	/**
+	 * @return the period of time
+	 */
+	Function<String, Long> Period = aPeriod -> {
+		String l_period = aPeriod;
+		int l_len = l_period.length();
+		long l_ret = 0;
+
+		if (l_len > 1) {
+			try {
+				String l_last = l_period.substring(l_len - 1, l_len);
+				long l_factor = 1;
+				int l_ind = "smhd".indexOf(l_last); //$NON-NLS-1$
+
+				switch (l_ind) {
+				case -1:
+					break;
+				case 0:
+					l_factor = 1000;
+					l_period = l_period.substring(0, l_len - 1);
+					break;
+				case 1:
+					l_factor = 60 * 1000;
+					l_period = l_period.substring(0, l_len - 1);
+					break;
+				case 2:
+					l_factor = 60 * 60 * 1000;
+					l_period = l_period.substring(0, l_len - 1);
+					break;
+				case 3:
+					l_factor = 24 * 60 * 60 * 1000;
+					l_period = l_period.substring(0, l_len - 1);
+					break;
+				default:
+					l_factor = 1;
+				}
+				l_ret = Long.valueOf(l_period);
+				l_ret = l_ret * l_factor;
+			} catch (Exception anEx) {
+				throw SYS.LOG.exception(anEx);
+			}
+		}
+		return l_ret;
+	};
 
 	/**
 	 * A wait in milliseconds.
